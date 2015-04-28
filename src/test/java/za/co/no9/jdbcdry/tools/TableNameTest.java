5849f08cc986a3b732f0a1badee00d2a06b9190a
Graeme Lockley <graemel@no9.co.za>
2015-04-28 10:02:49 +0200

    Added tests to confirm that the from was working as required.

---
+++ src/test/java/za/co/no9/jdbcdry/tools/TableNameTest.java
@@ -0,0 +1,38 @@
+package za.co.no9.jdbcdry.tools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class TableNameTest {
+    @Test
+    public void should_accept_single_element_in_qualified_name() throws Exception {
+        TableName tableName = TableName.from("BOB");
+
+        assertFalse(tableName.catalog().isPresent());
+        assertFalse(tableName.schema().isPresent());
+        assertEquals("BOB", tableName.name());
+        assertEquals("BOB", tableName.toString());
+    }
+
+    @Test
+    public void should_accept_two_elements_in_qualified_name() throws Exception {
+        TableName tableName = TableName.from("SCHEMA.BOB");
+
+        assertFalse(tableName.catalog().isPresent());
+        assertEquals("SCHEMA", tableName.schema().get());
+        assertEquals("BOB", tableName.name());
+        assertEquals("SCHEMA.BOB", tableName.toString());
+    }
+
+    @Test
+    public void should_accept_three_elements_in_qualified_name() throws Exception {
+        TableName tableName = TableName.from("CATALOG.SCHEMA.BOB");
+
+        assertEquals("CATALOG", tableName.catalog().get());
+        assertEquals("SCHEMA", tableName.schema().get());
+        assertEquals("BOB", tableName.name());
+        assertEquals("CATALOG.SCHEMA.BOB", tableName.toString());
+    }
+}
