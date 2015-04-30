e0b5a2d7afc4a39d9e18157d4b539330d70af68f
Graeme Lockley <graemel@no9.co.za>
2015-04-30 07:58:36 +0200

    Moved the collection class into a member variable.  This is to allow the drivers to become an adaptor and the models to be independent of connection.

--- src/main/java/za/co/no9/jdbcdry/model/DatabaseMetaData.java
+++ src/main/java/za/co/no9/jdbcdry/model/DatabaseMetaData.java
@@ -5,11 +5,14 @@ import za.co.no9.jdbcdry.drivers.DBDriver;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class DatabaseMetaData {
     private DBDriver dbDriver;
+    private Map<TableName, TableMetaData> tables = new HashMap<>();
 
     public DatabaseMetaData(DBDriver dbDriver) {
         this.dbDriver = dbDriver;
@@ -24,30 +27,30 @@ public class DatabaseMetaData {
     }
 
     private Stream<TableMetaData> tables(Optional<String> catalogue, Optional<String> schemaNamePattern, Optional<String> tableNamePattern) throws SQLException {
-        Map<TableName, TableMetaData> tables = allTablesDictionary(catalogue, schemaNamePattern, tableNamePattern);
-        return resolveForeignKeyConstraints(tables);
+        allTablesDictionary(catalogue, schemaNamePattern, tableNamePattern);
+        resolveForeignKeyConstraints(tables);
+        return tables.values().stream();
     }
 
-    private Map<TableName, TableMetaData> allTablesDictionary(Optional<String> catalogue, Optional<String> schemaNamePattern, Optional<String> tableNamePattern) throws SQLException {
-        Map<TableName, TableMetaData> tables = new HashMap<>();
-
+    private void allTablesDictionary(Optional<String> catalogue, Optional<String> schemaNamePattern, Optional<String> tableNamePattern) throws SQLException {
         java.sql.DatabaseMetaData metaData = getConnection().getMetaData();
         try (ResultSet rs = metaData.getTables(catalogue.orElse(null), schemaNamePattern.orElse(null), tableNamePattern.orElse(null), null)) {
             while (rs.next()) {
                 TableMetaData tableMetaData = dbDriver.tableMetaData(TableName.from(rs.getString(1), rs.getString(2), rs.getString(3)));
-                tables.put(tableMetaData.tableName(), tableMetaData);
+                addTable(tableMetaData);
             }
         }
-        return tables;
     }
 
-    private Stream<TableMetaData> resolveForeignKeyConstraints(Map<TableName, TableMetaData> tables) throws SQLException {
-        List<TableMetaData> result = new ArrayList<>();
+    private void resolveForeignKeyConstraints(Map<TableName, TableMetaData> tables) throws SQLException {
         for (TableMetaData tableMetaData : tables.values()) {
-            result.add(dbDriver.resolveForeignConstraints(tables, tableMetaData));
+            addTable(dbDriver.resolveForeignConstraints(tables, tableMetaData));
         }
-        return result.stream();
     }
 
     private Connection getConnection() { return dbDriver.getConnection(); }
+
+    private void addTable(TableMetaData table) {
+        tables.put(table.tableName(), table);
+    }
 }
